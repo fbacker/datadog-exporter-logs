36d5a425c34dce6fcc201fcd9f47bada1da88658
Fredrick Bäcker <fredrick.backer@folkspel.se>
2022-05-06 12:43:54 +0200

    retry wait longer

--- DatadogLogsExporter/policies/HTTPClientPolicy.cs
+++ DatadogLogsExporter/policies/HTTPClientPolicy.cs
@@ -12,7 +12,7 @@ namespace DatadogLogsExporter.Policies
             ImmediateHttpRetry = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
                                     .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
                                     .WaitAndRetryAsync(3, 
-                                        retryAttempt => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt)),
+                                        retryAttempt => TimeSpan.FromSeconds(Math.Pow(4, retryAttempt)),
                                         (exception, retryCount) =>
                                         {
                                             Console.WriteLine($"{DateTime.Now} Failed http call retry delay: {retryCount}, response code: {exception.Result.StatusCode}");
